e16614167458c4bc9419c8c538ba7a7c6c829023
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-08-02 00:17:53 -0300

    Update deploy 1/8

---
+++ app/maintenance.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Player } from "@lottiefiles/react-lottie-player";
+import Maintenance from "../public/icons/maintenance.json";
+
+const MaintenancePage = () => {
+  return (
+    <Player
+      autoplay
+      loop
+      src={Maintenance}
+      className="w-96 h-96"
+    />
+  )
+};
+
+export default MaintenancePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,45 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import ThemeSwitch from "./themeSwitch";
 import { HomeLink } from "./homeLink";
 import { AboutLink } from "./aboutLink";
 import { ContactLink } from "./contactLink";
+import MaintenancePage from "./maintenance";
+
 
 const Landing = () => {
 
   return (
-    <main className="w-full h-screen bg-[--bgd]">
-      <nav className="navbar h-[10%] bg-[--pri] sticky top-0">
-        <section className="navbar-start">
+    <main className="w-full min-h-screen bg-[--bgd] flex-col justify-between items-center">
+      <nav className="navbar h-[10%] border-b border-[--pri] backdrop-filter backdrop-blur-md z-10">
+        <section className="navbar-start"></section>
+        <section className="navbar-center w-11/12 flex-row justify-between items-center">
           <span>Agustin Cabral</span>
-        </section>
-        <section className="navbar-end flex-row justify-around items-center">
           <HomeLink />
           <AboutLink />
           <ContactLink />
           <ThemeSwitch />
         </section>
+        <section className="navbar-end"></section>
       </nav>
-      <section className="w-full h-[85%]">
-        <article className="w-full h-full flex-col justify-around items-center border">
-          <div className="w-64 h-80 relative">
-            <Image src={"/aguscabral.jpg"} alt="photo" fill={true} className="absolute object-cover rounded-box" />
-          </div>
-          <div>
-            <h1 className="text-5xl">Hola! Soy Agustin</h1>
-            <h2 className="">I'm a <span className="font-bold">FULLSTACK DEVELOPER</span></h2>
-          </div>
-        </article>
+      <section className="w-full min-h-[90svh] grid place-content-center">
+        <MaintenancePage />
       </section>
-      <footer className="w-full h-[5%] border-t-2">
-
+      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <div className="grid grid-flow-col gap-4">
+          <a className="link link-hover">About</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+        </div>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+          </div>
+        </div>
+        <div>
+          <p>Copyright © 2023 - aguscabralr</p>
+        </div>
       </footer>
     </main>
   )
